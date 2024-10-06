2eac224e5baf1b9fbc744a253efc8eb0e6042a27
Santananda <chaudharysantanand@gmail.com>
2024-10-07 03:00:52 +0545

    we make a some file and folders

--- src/pages/blog/Home.jsx
+++ src/pages/blog/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Layout from '../../components/navbar/Layout'
+import Card from './components/card/Card'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+     <Layout >
+      <div className='flex flex-wrap justify-center space-x-5 mt-6 mb-6'>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+
+      </div>
+     </Layout>
   )
 }
 
